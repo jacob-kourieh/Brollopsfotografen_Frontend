bc9c4f4cba60df88cae4fe15814555abbf520c89
jacob-kourieh <you@example.com>
2022-09-27 23:46:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import StartPage from "./pages/StartPage";
+import LogInPage from "./pages/LogInPage";
+import SignupPage from "./pages/SignupPage";
+import TakenPhotoPage from "./pages/TakenPhotoPage";
+import CameraPage from "./pages/CameraPage";
+import AlbumPage from "./pages/AlbumPage";
+import AdminPage from "./pages/AdminPage";
+import AdminAlbumPage from "./pages/AdminAlbumPage";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/skapakonto" element={<SignupPage />} />
+          <Route path="/loggain" element={<LogInPage />} />
+          <Route path='/CameraPage' element={<CameraPage />} />
+          <Route path='/TakenPhotoPage' element={<TakenPhotoPage />} />
+          <Route path='/AlbumPage' element={<AlbumPage />} />
+          <Route path='/Admin' element={<AdminPage />} />
+          <Route path='/AdminAlbum' element={<AdminAlbumPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
